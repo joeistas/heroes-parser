db91f96bb8bd470d718d34609e8f034526460b88
Joe Istas <joe.istas@istastech.com>
2018-04-19 17:03:50 -0500

    Added parser to support merging from inner element

--- src/parsers/merge-parsers.ts
+++ src/parsers/merge-parsers.ts
@@ -11,14 +11,13 @@ import { ElementNameFilter } from './element-name-filters'
 import { preParseElement, ElementParser } from './'
 
 export function mergeElement(elementNameOrFilter: string | ElementNameFilter, attribute: string = 'value'): ElementParser {
-  return (element: any, containingElement: any, parseData: ParseData): any => {
+  return (element: any, outerElement: any, parseData: ParseData): any => {
     const attributes = getElementAttributes(element)
     if(attributes[attribute] === undefined) {
       return element
     }
 
-    const elementNames = typeof elementNameOrFilter === 'string' ? [ elementNameOrFilter ] : elementNameOrFilter(parseData)
-    const elementName = findElementNameForId(elementNames, attributes[attribute], parseData.elements)
+    const elementName = findElementName(elementNameOrFilter, attributes[attribute], parseData)
     if(!elementName) {
       return element
     }
@@ -26,6 +25,42 @@ export function mergeElement(elementNameOrFilter: string | ElementNameFilter, at
     let replacementElement = joinElements(getElement(attributes[attribute], elementName, parseData.elements))
     replacementElement = mergeElements(element, replacementElement, parseData, ATTRIBUTE_BLACKLIST.concat(attribute))
 
-    return preParseElement(replacementElement, containingElement, elementName, parseData)
+    return preParseElement(replacementElement, outerElement, elementName, parseData)
   }
+}
+
+export function mergeElementFromInnerElementValue(
+  elementNameOrFilter: string | ElementNameFilter,
+  innerElementName: string,
+  attribute: string = 'value',
+  removeInnerElements: boolean = true
+): ElementParser {
+  return (element: any, outerElement: any, parseData: ParseData): any => {
+    const innerElement = preParseElement(element[innerElementName][0], outerElement, innerElementName, parseData)
+    const innerElementAttributes = getElementAttributes(innerElement)
+
+    const elementId = innerElementAttributes[attribute]
+    if(!elementId) {
+      return element
+    }
+
+    const elementName = findElementName(elementNameOrFilter, elementId, parseData)
+    if(!elementName) {
+      return element
+    }
+
+    if(removeInnerElements) {
+      delete element[innerElementName]
+    }
+
+    let replacementElement = joinElements(getElement(elementId, elementName, parseData.elements))
+    replacementElement = mergeElements(element, replacementElement, parseData)
+
+    return preParseElement(replacementElement, outerElement, elementName, parseData)
+  }
+}
+
+function findElementName(elementNameOrFilter: string | ElementNameFilter, elementId: string, parseData: ParseData): string {
+  const elementNames = typeof elementNameOrFilter === 'string' ? [ elementNameOrFilter ] : elementNameOrFilter(parseData)
+  return findElementNameForId(elementNames, elementId, parseData.elements)
 }
--- test/parsers/merge-parsers.spec.ts
+++ test/parsers/merge-parsers.spec.ts
@@ -2,7 +2,7 @@ import { expect } from 'chai'
 import * as sinon from 'sinon'
 
 import { ELEMENT_NAME_KEY, ELEMENT_ATTRIBUTE_KEY } from '../../src/element'
-import { mergeElement } from '../../src/parsers/merge-parsers'
+import { mergeElement, mergeElementFromInnerElementValue } from '../../src/parsers/merge-parsers'
 
 describe("mergeElement", function() {
   const parseData: any = {
@@ -44,11 +44,11 @@ describe("mergeElement", function() {
           'Skin',
           new Map([
             [
-              'MuratinUltimate',
+              'MuradinUltimate',
               [
                 {
                   [ELEMENT_ATTRIBUTE_KEY]: {
-                    id: "MuratinUltimate",
+                    id: "MuradinUltimate",
                   }
                 }
               ]
@@ -185,5 +185,230 @@ describe("mergeElement", function() {
 
     const result = mergeElement('Hero', 'value')(element, {}, parseData)
     expect(result[ELEMENT_ATTRIBUTE_KEY]).to.not.have.property('value')
+  })
+})
+
+describe("mergeElementFromInnerElementValue", function() {
+  const parseData: any = {
+    functions: {
+      default: {
+        merge: (parentElements: any[], childElements: any[]) => parentElements.concat(childElements)
+      }
+    },
+    elements: new Map(
+      [
+        [
+          'Skin',
+          new Map([
+            [
+              'MuradinUltimate',
+              [
+                {
+                  [ELEMENT_ATTRIBUTE_KEY]: {
+                    id: "MuradinUltimate",
+                  }
+                }
+              ]
+            ]
+          ])
+        ],
+      ]
+    )
+  }
+
+  it("should not merge the element if attribute on the inner element is undefined", function() {
+    const element: any = {
+      [ELEMENT_ATTRIBUTE_KEY]: {
+        value: 'thing',
+      },
+
+      Variation: [
+        {
+          [ELEMENT_ATTRIBUTE_KEY]: {
+            value: undefined,
+          }
+        }
+      ]
+    }
+
+    const result = mergeElementFromInnerElementValue('Skin', "Variation", 'test')(element, {}, parseData)
+    expect(result).to.eql(element)
+  })
+
+  it("should not merge the element if a matching element cannot be found in the ElementMap", function() {
+    const element: any = {
+      [ELEMENT_ATTRIBUTE_KEY]: {
+        value: 'thing',
+      },
+
+      Variation: [
+        {
+          [ELEMENT_ATTRIBUTE_KEY]: {
+            value: "MuradinMagni",
+          }
+        }
+      ]
+    }
+
+    const result = mergeElementFromInnerElementValue('Skin', 'Variation', 'value')(element, {}, parseData)
+    expect(result).to.eql(element)
+  })
+
+  it("should merge the element if elementNameOrFilter is a string", function() {
+    const element: any = {
+      [ELEMENT_ATTRIBUTE_KEY]: {
+        value: 'thing',
+      },
+
+      Variation: [
+        {
+          [ELEMENT_ATTRIBUTE_KEY]: {
+            value: "MuradinUltimate",
+          }
+        }
+      ]
+    }
+
+    const result = mergeElementFromInnerElementValue('Skin', 'Variation', 'value', false)(element, {}, parseData)
+    expect(result).to.eql(
+      {
+        [ELEMENT_ATTRIBUTE_KEY]: {
+          id: 'MuradinUltimate',
+          value: 'thing',
+        },
+        [ELEMENT_NAME_KEY]: 'Skin',
+        Variation: [
+          {
+            [ELEMENT_ATTRIBUTE_KEY]: {
+              value: "MuradinUltimate",
+            },
+            [ELEMENT_NAME_KEY]: 'Variation',
+          }
+        ]
+      }
+    )
+  })
+
+  it("should merge the element if elementNameOrFilter is an ElementNameFilter", function() {
+    const element: any = {
+      [ELEMENT_ATTRIBUTE_KEY]: {
+        value: 'thing',
+      },
+
+      Variation: [
+        {
+          [ELEMENT_ATTRIBUTE_KEY]: {
+            value: "MuradinUltimate",
+          }
+        }
+      ]
+    }
+
+    const result = mergeElementFromInnerElementValue(() => [ 'Skin' ], 'Variation', 'value', false)(element, {}, parseData)
+    expect(result).to.eql(
+      {
+        [ELEMENT_ATTRIBUTE_KEY]: {
+          id: 'MuradinUltimate',
+          value: 'thing',
+        },
+        [ELEMENT_NAME_KEY]: 'Skin',
+        Variation: [
+          {
+            [ELEMENT_ATTRIBUTE_KEY]: {
+              value: "MuradinUltimate",
+            },
+            [ELEMENT_NAME_KEY]: 'Variation',
+          }
+        ]
+      }
+    )
+  })
+
+  it("should call 'preParse' on the new merged element", function() {
+    const element: any = {
+      [ELEMENT_ATTRIBUTE_KEY]: {
+        value: 'thing',
+      },
+
+      Variation: [
+        {
+          [ELEMENT_ATTRIBUTE_KEY]: {
+            value: "MuradinUltimate",
+          }
+        }
+      ]
+    }
+
+    const data = {
+      ...parseData,
+      functions: {
+        ...parseData.functions,
+        'Skin': {
+          preParse: sinon.spy()
+        }
+      }
+    }
+
+    mergeElementFromInnerElementValue('Skin', 'Variation', 'value')(element, {}, data)
+    expect(data.functions.Skin.preParse).to.have.been.called
+  })
+
+  it("should set 'elementName' to the merged element's name", function() {
+    const element: any = {
+      [ELEMENT_ATTRIBUTE_KEY]: {
+        value: 'thing',
+      },
+
+      Variation: [
+        {
+          [ELEMENT_ATTRIBUTE_KEY]: {
+            value: "MuradinUltimate",
+          }
+        }
+      ]
+    }
+
+    const result = mergeElementFromInnerElementValue('Skin', 'Variation', 'value')(element, {}, parseData)
+    expect(result).to.have.property(ELEMENT_NAME_KEY).that.is.eql('Skin')
+  })
+
+  it("should remove the innerElements from the result if 'removeInnerElements' is true", function() {
+    const element: any = {
+      [ELEMENT_ATTRIBUTE_KEY]: {
+        value: 'thing',
+      },
+
+      Variation: [
+        {
+          [ELEMENT_ATTRIBUTE_KEY]: {
+            value: "MuradinUltimate",
+          }
+        }
+      ]
+    }
+
+    const result = mergeElementFromInnerElementValue('Skin', 'Variation', 'value', true)(element, {}, parseData)
+
+    expect(result).to.not.have.property('Variation')
+  })
+
+  it("should have innerElements in the result if 'removeInnerElements' is false", function() {
+    const element: any = {
+      [ELEMENT_ATTRIBUTE_KEY]: {
+        value: 'thing',
+      },
+
+      Variation: [
+        {
+          [ELEMENT_ATTRIBUTE_KEY]: {
+            value: "MuradinUltimate",
+          }
+        }
+      ]
+    }
+
+    const result = mergeElementFromInnerElementValue('Skin', 'Variation', 'value', false)(element, {}, parseData)
+
+    expect(result).to.have.property('Variation')
   })
 })
